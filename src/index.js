d80adb7656a448b84b7c4f061e6471c644d6cdb4
unknown <shivanshkumar166@gmail.com>
2020-12-28 16:04:20 +0530

    basic structure

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/App'
- ReactDOM.render(<App />,document.querySelector('#root'))
+import App from './component/App'
+ ReactDOM.render(<App />,document.querySelector('#root'))  
