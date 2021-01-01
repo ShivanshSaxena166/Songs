65f5af60b224d09969fbc524bb78fddec2feb912
unknown <shivanshkumar166@gmail.com>
2021-01-01 17:52:38 +0530

    provider

--- src/component/App.js
+++ src/component/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import SongList from './SongList'
+import {selectSong} from '../actions/index'
+
 const App=()=>{
-    return <div>App</div>
+    return <div><SingList /></div>
 }
 export default App;
---
+++ src/component/SongList.js
@@ -0,0 +1,8 @@
+import React,{Components} from 'react';
+class SongList extends Component{
+    render()
+    {
+        return <div> SongList</div>
+    }
+
+}
